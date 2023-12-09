04791b946fcb7caf194a50279a6e6802b2d48f99
aakash-sharma25 <as024harma@gmail.com>
2023-12-09 11:57:02 +0530

    Changes in MiddleWare file

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -3,52 +3,42 @@ const User = require("../model/userModel");
 
 //protected routes token based
 
-exports.requireSignIn = async(req,res,next) => {
-    
-    try {
+exports.requireSignIn = async (req, res, next) => {
+  try {
+    const decode = JWT.verify(
+      req.headers.authorization,
+      process.env.JWT_SECRET
+    );
+    req.user = decode;
 
-    const decode = JWT.verify(req.headers.authorization , process.env.JWT_SECRET);
-    req.user = decode ;
     next();
-        
-    } catch (error) {
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: "error in authorization",
+      error: error.message,
+    });
+  }
+};
 
-        console.log(error);
-        res.status(500).json({
-            success:false,
-            message:"error in authorization",
-            error:error.message,
-        })
-        
-        
+exports.isAdmin = async (req, res, next) => {
+  try {
+    const user = await User.findById(req.user._id);
+
+    if (user?.role !== 1) {
+      res.status(404).json({
+        success: false,
+        message: "You Are Not a Admin ",
+      });
+    } else {
+      next();
     }
-} 
-
-exports.isAdmin = async(req,res,next) => {
-
-    try {
-
-        const user = await User.findById(req.user._id);
-        // console.log(user)
-
-        if(user?.role !==1) {
-            res.status(404).json({
-                success:false,
-                message:"you are not admin ",
-            })
-        }
-        else {
-            next();
-        }
-        
-    } catch (error) {
-
-        console.log(error);
-        res.status(500).json({
-            success:false,
-            message:"this is protected route for admin",
-            error:error.message,
-        })
-        
-    }
-}
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "This is protected route for admin",
+      error: error.message,
+    });
+  }
+};
